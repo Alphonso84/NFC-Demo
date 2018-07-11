989d6927e3b31b4b70af5d15fe6d185e9d0553e7
Alphonso Bret Sensley II <fonzman1984@me.com>
2018-07-10 21:06:05 -0700

    First Commit

--- NFC Demo/ViewController.swift
+++ NFC Demo/ViewController.swift
@@ -7,9 +7,27 @@
 //
 
 import UIKit
+import CoreNFC
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, NFCNDEFReaderSessionDelegate {
+
+    @IBOutlet weak var messageLabel: UILabel!
+    var nfcSession: NFCNDEFReaderSession?
+    
+    @IBAction func scanWasPressed(_ sender: Any) {
+        
+        
+    }
+    
+    func readerSession(_ session: NFCNDEFReaderSession, didInvalidateWithError error: Error) {
+        print("The session was invalidated: \(error.localizedDescription)")
+    }
+    
+    func readerSession(_ session: NFCNDEFReaderSession, didDetectNDEFs messages: [NFCNDEFMessage]) {
+        // Parse the card's information
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
